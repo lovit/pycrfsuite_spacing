cf555eecf80fd922a1632c8009fa27344fff3cb5
lovit <soy.lovit@gmail.com>
2017-09-30 19:51:16 +0900

    Add scan_feature_vocabulary avoiding append useless feature

--- pycrfsuite_spacing/tagger.py
+++ pycrfsuite_spacing/tagger.py
@@ -3,10 +3,16 @@ from .transform import docs_to_xy
 from .transform import sent_to_xy
 
 class PyCRFSuiteSpacing:
-    def __init__(self, to_feature, tagger=None, verbose=False):
+    def __init__(self, to_feature, tagger=None, verbose=False,
+                 feature_vocabulary=None,
+                 feature_minfreq=0, max_iterations=100):
         self.tagger = tagger
         self.to_feature = to_feature
         self.verbose = verbose
+        self.feature_vocabulary = feature_vocabulary
+        self.params = {'feature.minfreq':max(0,feature_minfreq),
+                       'max_iterations':max(1, max_iterations)
+                      }
         
         if type(tagger) == 'str':
             try:
@@ -18,16 +24,41 @@ class PyCRFSuiteSpacing:
     def __call__(self, sent):
         return self.correct(sent)
     
-    def train(self, docs, model_fname):        
-        train_x, train_y = docs_to_xy(docs, self.to_feature)
+    def train(self, docs, model_fname):
+        if not self.feature_vocabulary:
+            self.feature_vocabulary = self._scan_features(docs)
         trainer = pycrfsuite.Trainer(verbose=self.verbose)
-        for x, y in zip(train_x, train_y):
+        if self.verbose:
+            print('begin appending data to trainer')
+        for sent in docs:
+            x, y = sent_to_xy(sent, self.to_feature)
             if len(x) != len(y):
                 continue
+            x = [[xij for xij in xi if xij in self.feature_vocabulary] for xi in x]
             trainer.append(x, y)
+        if self.verbose:
+            print('all data are appended to trainer. begin training')
+        trainer.set_params(self.params)
         trainer.train(model_fname)
         self.load_tagger(model_fname)
     
+    def _scan_features(self, docs):
+        min_count = self.params['feature.minfreq']
+        if self.verbose:
+            print('feature scanning: begin with min_count={}'.format(min_count))
+            
+        from collections import Counter
+        train_x, _ = docs_to_xy(docs, self.to_feature)
+        feature_vocabulary = Counter((xij for x in train_x for xi in x for xij in xi))        
+        if self.verbose:
+            print('feature scanning ... {} -> '.format(len(feature_vocabulary)), end='')
+            
+        feature_vocabulary = {feature for feature, count in feature_vocabulary.items() if count >= min_count}
+        if self.verbose:
+            print('{} with min_count={}'.format(len(feature_vocabulary), min_count))
+            
+        return feature_vocabulary
+        
     def load_tagger(self, model_fname):
         self.tagger = pycrfsuite.Tagger()
         self.tagger.open(model_fname)
